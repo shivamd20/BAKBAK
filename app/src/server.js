643b85ca39048cc15dfb868f581c623fa44a0188
Shivam Dwivedi <shivamd20@gmail.com>
2017-07-23 16:04:27 +0530

    added Facebook.htm

--- app/src/server.js
+++ app/src/server.js
@@ -2,6 +2,13 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
 //your routes here
 app.get('/', function (req, res) {
     res.send("Hello World! how are you");
