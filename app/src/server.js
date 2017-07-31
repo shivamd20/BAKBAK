8ec5d80da21f46fc2d630571008b641435436c03
Shivam Dwivedi <shivamd20@gmail.com>
2017-08-01 00:00:41 +0530

    finally account kit intrigrated

--- app/src/server.js
+++ app/src/server.js
@@ -29,13 +29,12 @@ var existinguser = function (req, res,mobile) {
 
   var loginUrl = url + '/login';
 
-
   var optionsLogin = {
     method: 'POST',
     headers,
     body: JSON.stringify({
       "mobile": mobile,
-      "password": "123456"
+      "password": defaultPassword+mobile
     })
   };
 
@@ -52,8 +51,6 @@ var existinguser = function (req, res,mobile) {
             return true;
           }
           else if (response.status == 403) {
-
-            //do sign up and return the respons        fetch()
 
             res.send("" + 403);
           }
@@ -73,17 +70,12 @@ var existinguser = function (req, res,mobile) {
     },
     (e) => {
       console.error(e);
-      res.status = 403;
       res.send('Error in fetching current schema: ' + e.toString());
     })
     .catch((e) => {
       e.stackTrace();
-      res.status = 403;
       res.send('Error in fetching current schema: ' + e.toString());
     });
-
-  return 525;
-
 };
 
 var signup = function (req, res,mobile) {
@@ -93,7 +85,7 @@ var signup = function (req, res,mobile) {
     headers,
     body: JSON.stringify({
       "mobile": mobile,
-      "password": "123456",
+      "password": defaultPassword+mobile,
       "username":mobile+"@chitchat"
     })
   };
@@ -113,31 +105,7 @@ var signup = function (req, res,mobile) {
             return true;
           }
           else if (response.status == 409) {
-
-            //do sign up and return the respons        fetch()
-
               existinguser(req,res,mobile);
-
-            // fetch(signupUrl,Option).then((responseSinup)=>{
-            //   responseSinup.text().then((datasinup)=>{
-
-            //      if(response.status==200)
-            // {
-            //   var d=JSON.parse(data);
-            //   res.send("data"+d);
-            // }else
-            // {
-            //   res.status=403;
-            //   res.send("error");
-            // }
-            //   })
-            // },(e)=>{
-            //     res.send("error");
-            // })
-            // .catch((e)=>{
-            //     res.send("error");
-            // });
-
           }
           else {
 
@@ -145,9 +113,6 @@ var signup = function (req, res,mobile) {
             
             res.send(response.status + "" + data);
           }
-
-
-
         },
         (e) => {
           res.status = 403;
@@ -169,38 +134,63 @@ var signup = function (req, res,mobile) {
       res.status = 403;
       res.send('Error in fetching current schema: ' + e.toString());
     });
-
   return 525;
-
 };
 
+var fbaccountKit=function(req,res,accessToken){
+
+var graphUrl="https://graph.accountkit.com/v1.2/me/?access_token="+accessToken;
+fetch(graphUrl)
+    .then(
+    (response) => {
+      response.text()
+        .then(
+        (data) => {
+          res.status=response.status;
+          if (response.status == 200) {
+            var d = JSON.parse(data);
+            
+            signup(req,res,d.phone.national_number);
+
+            return true;
+          }
+          else {
+            res.status=response.status;
+            res.send(response.status + "" + data);
+          }
+        },
+        (e) => {
+          res.status = 403;
+          res.send('Error in fetching current schema: ' + err.toString());
+        })
+        .catch((e) => {
+          e.stack();
+          res.status = 403;
+          res.send('Error in fetching current schema: ' + e.toString());
+        });
+    },
+    (e) => {
+      console.error(e);
+      res.status = 403;
+      res.send('Error in fetching current schema: ' + e.toString());
+    })
+    .catch((e) => {
+      e.stackTrace();
+      res.status = 403;
+      res.send('Error in fetching current schema: ' + e.toString());
+    });
+ 
+ 
+
+}
+
 app.get('/', function (req, res) {
-  //existinguser(req, res);
+//signup(req,res,req.query.mobile);
 
-signup(req,res,req.query.mobile);
+fbaccountKit(req,res,req.query.access_token);
 
-  //TODO VARIFY MOBILE NUMBER FOR LOGIN
 });
 
-/*
- * Sample endpoint to check the role of a user
- * When any user makes a request to this endpoint with
- * the path containing the roleName. Eg: /admin, /user, /anonymous
- * that path only gets served, if the user actually has that role.
- * To test, login to the console as an admin user. /admin, /user will work.
- * Make a request to /admin, /user from an incognito tab. They won't work, only /anonymous will work.
- */
-app.get('/:role', function (req, res) {
-  var roles = req.get('X-Hasura-Allowed-Roles');
-
-  // Check if allowed roles contains the rolename mentioned in the URL
-  if (roles.indexOf(req.params.role) > -1) {
-    res.send('Hey, you have the <b>' + req.params.role + '</b> role');
-  } else {
-    res.status(403).send('DENIED: Only a user with the role <b>' + req.params.role + '</b> can access this endpoint');
-  }
-});
-
-app.listen(8088, function () {
+app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
